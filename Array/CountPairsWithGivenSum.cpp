600a588607584d33e49daa4650ef7cf6280cf395
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-12-13 15:18:48 +0530

    New problem

---
+++ Array/CountPairsWithGivenSum.cpp
@@ -0,0 +1,375 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Select Language
+Refresh
+
+Time (IST)	Status	User	Lang	Code
+2022-12-13 15:14:25	Wrong	khansohatz4k	C++	View
+2022-12-13 15:14:13	Wrong	khansohatz4k	C++	View
+2022-12-13 15:12:35	Wrong	gunjanagarwal088	C++	View
+2022-12-13 15:10:25	TLE	manikafehq	C++	View
+2022-12-13 15:05:05	Correct	venkataramsai72	Java	View
+2022-12-13 15:00:15	TLE	sheetalgour04	Python3	View
+2022-12-13 14:55:45	Correct	shahrusavd2	C++	View
+2022-12-13 14:46:30	TLE	ujjwalguw3tu	Java	View
+2022-12-13 14:45:47	Correct	ridresesbh	Python3	View
+2022-12-13 14:44:15	Correct	csc212d7q	Java	View
+2022-12-13 14:44:03	Compilation Error	csc212d7q	Java	View
+2022-12-13 14:43:41	Wrong	csc212d7q	Java	View
+2022-12-13 14:42:36	Wrong	eduomo8ld	C++	View
+2022-12-13 14:41:41	Wrong	csc212d7q	Java	View
+2022-12-13 14:39:50	TLE	eduomo8ld	C++	View
+2022-12-13 14:34:49	Wrong	ridresesbh	Python3	View
+2022-12-13 14:32:10	TLE	ridresesbh	Python3	View
+2022-12-13 14:28:11	TLE	csc212d7q	Java	View
+2022-12-13 14:28:01	Compilation Error	csc212d7q	Java	View
+2022-12-13 14:27:59	TLE	shivam_gg	C++	View
+2022-12-13 14:27:45	TLE	csc212d7q	Java	View
+2022-12-13 14:27:14	TLE	csc212d7q	Java	View
+2022-12-13 14:17:17	TLE	yashbhanni6i	Python3	View
+2022-12-13 14:08:18	TLE	donthupranavi	Java	View
+2022-12-13 14:06:37	TLE	radhashyam9321	Java	View
+2022-12-13 13:49:31	TLE	yashbhanni6i	Python3	View
+2022-12-13 13:44:05	Correct	prathamangdikar7227	Java	View
+2022-12-13 13:36:29	TLE	sarthxk2003	C++	View
+2022-12-13 13:33:21	Correct	ajaybandela441	Java	View
+2022-12-13 13:32:22	Correct	shreyaaaaa262	C++	View
+2022-12-13 13:31:36	TLE	ajaybandela441	Java	View
+2022-12-13 13:30:35	Correct	kumarravi212002	C++	View
+2022-12-13 13:20:20	Correct	kumarravi212002	C++	View
+2022-12-13 13:20:18	Correct	kumarravi212002	C++	View
+2022-12-13 13:19:44	Wrong	kumarravi212002	C++	View
+2022-12-13 13:15:18	Correct	saurabhpardhi	C++	View
+2022-12-13 13:14:53	Wrong	saurabhpardhi	C++	View
+2022-12-13 13:13:22	TLE	amithhh1234	Java	View
+2022-12-13 13:12:53	Wrong	amithhh1234	Java	View
+2022-12-13 13:08:49	Wrong	kumarravi212002	C++	View
+2022-12-13 13:08:36	TLE	amithhh1234	Java	View
+2022-12-13 13:07:50	TLE	dragontruoki	Python3	View
+2022-12-13 13:06:40	TLE	dragontruoki	Python3	View
+2022-12-13 13:06:15	TLE	helibhing0ept	Java	View
+2022-12-13 13:04:39	Correct	sujeetsio6rn	Python3	View
+2022-12-13 13:00:11	Wrong	sujeetsio6rn	Python3	View
+2022-12-13 12:56:05	Wrong	helibhing0ept	Java	View
+2022-12-13 12:50:52	Correct	SourabhSharma8	Java	View
+2022-12-13 12:50:02	Wrong	krishnaharplxm	Python3	View
+2022-12-13 12:49:32	TLE	kumarravi212002	C++	View
+2022-12-13 12:49:15	TLE	himanshudhingra62	Java	View
+2022-12-13 12:45:43	TLE	aniketkushcwo0	C++	View
+2022-12-13 12:44:42	TLE	radhashyam9321	Java	View
+2022-12-13 12:34:31	TLE	radhashyam9321	Java	View
+2022-12-13 12:33:00	Wrong	helibhing0ept	Java	View
+2022-12-13 12:32:47	Wrong	helibhing0ept	Java	View
+2022-12-13 12:31:55	Wrong	helibhing0ept	Java	View
+2022-12-13 12:31:13	Correct	prashantk2singh	Python3	View
+2022-12-13 12:29:37	Wrong	harshmah	Java	View
+2022-12-13 12:28:32	RunTime Error	helibhing0ept	Java	View
+2022-12-13 12:28:28	Wrong	harshmah	Java	View
+2022-12-13 12:27:27	Wrong	harshmah	Java	View
+2022-12-13 12:26:20	Wrong	helibhing0ept	Java	View
+2022-12-13 12:25:31	TLE	himanshudhingra62	Java	View
+2022-12-13 12:22:09	Correct	UmakantaPradhan	Java	View
+2022-12-13 12:22:03	Wrong	arshjitsi6nij	C++	View
+2022-12-13 12:22:01	Wrong	helibhing0ept	Java	View
+2022-12-13 12:21:15	Correct	pkp20022	C++	View
+2022-12-13 12:20:53	Wrong	goyalyash311	C++	View
+2022-12-13 12:20:14	Correct	harshmah	Java	View
+2022-12-13 12:17:21	TLE	UmakantaPradhan	Java	View
+2022-12-13 12:16:26	Wrong	helibhing0ept	Java	View
+2022-12-13 12:15:45	Wrong	helibhing0ept	Java	View
+2022-12-13 12:15:28	Wrong	helibhing0ept	Java	View
+2022-12-13 12:15:13	Wrong	helibhing0ept	Java	View
+2022-12-13 12:07:45	Wrong	helibhing0ept	Java	View
+2022-12-13 12:07:13	Wrong	helibhing0ept	Java	View
+2022-12-13 12:06:32	TLE	UmakantaPradhan	Java	View
+2022-12-13 12:06:19	Wrong	helibhing0ept	Java	View
+2022-12-13 12:05:34	Wrong	helibhing0ept	Java	View
+2022-12-13 12:02:43	Correct	sahil18official	C++	View
+2022-12-13 11:40:22	TLE	kumarravi212002	C++	View
+2022-12-13 11:38:53	Wrong	chongala_jaiteja1234	C++	View
+2022-12-13 11:30:45	Correct	dchinna8985	C++	View
+2022-12-13 11:10:57	Wrong	akashverma8331	C++	View
+2022-12-13 10:59:09	Correct	adityaakangire2000	C++	View
+2022-12-13 10:57:10	Wrong	adityaakangire2000	C++	View
+2022-12-13 10:54:01	Correct	pankajwaykar123	C++	View
+2022-12-13 10:53:24	Wrong	basha	C++	View
+2022-12-13 10:51:40	Wrong	basha	C++	View
+2022-12-13 10:51:25	Wrong	basha	C++	View
+2022-12-13 10:51:03	Wrong	basha	C++	View
+2022-12-13 10:50:29	Wrong	basha	C++	View
+2022-12-13 10:49:30	Wrong	basha	C++	View
+2022-12-13 10:48:41	Wrong	basha	C++	View
+2022-12-13 10:47:15	Correct	bhavanicclh	Java	View
+2022-12-13 10:41:08	Wrong	bhavanicclh	Java	View
+2022-12-13 10:27:15	Correct	kevinlang9981	Java	View
+2022-12-13 10:26:38	Wrong	kevinlang9981	Java	View
+2022-12-13 10:26:29	Compilation Error	kevinlang9981	Java	View
+2022-12-13 10:21:13	TLE	pankajwaykar123	C++	View
+2022-12-13 10:20:17	Correct	shivangisingh2021	Java	View
+2022-12-13 10:18:42	Wrong	pankajwaykar123	C++	View
+2022-12-13 10:12:24	Wrong	pankajwaykar123	C++	View
+2022-12-13 10:09:04	Compilation Error	abhishekchauhanbca2019	Java	View
+2022-12-13 10:04:45	TLE	hsthakuv8d6	Java	View
+2022-12-13 09:56:56	Wrong	basha	C++	View
+2022-12-13 09:55:36	Wrong	basha	C++	View
+2022-12-13 09:49:59	Wrong	basha	C++	View
+2022-12-13 09:49:31	Wrong	basha	C++	View
+2022-12-13 09:46:14	Wrong	ab44891	C++	View
+2022-12-13 09:19:33	Correct	richardnova1999	Javascript	View
+2022-12-13 09:18:52	Correct	venomicrumxos	C++	View
+2022-12-13 09:12:45	Correct	bruhmantri	C++	View
+2022-12-13 09:07:13	Wrong	bruhmantri	C++	View
+2022-12-13 09:04:04	Wrong	bruhmantri	C++	View
+2022-12-13 08:58:46	Wrong	kevinlang9981	Java	View
+2022-12-13 08:55:46	Wrong	shreyaaaaa262	C++	View
+2022-12-13 08:38:30	Wrong	kevinlang9981	Java	View
+2022-12-13 08:38:03	Correct	namannr9	C++	View
+2022-12-13 08:35:45	TLE	namannr9	C++	View
+2022-12-13 08:35:36	Wrong	kevinlang9981	Java	View
+2022-12-13 08:35:12	Wrong	kevinlang9981	Java	View
+2022-12-13 08:34:59	Correct	chrisdi0f0x	C++	View
+2022-12-13 08:34:29	Correct	vinaysalpa	C++	View
+2022-12-13 08:31:46	Wrong	vinaysalpa	C++	View
+2022-12-13 08:26:14	Wrong	shreyaaaaa262	C++	View
+2022-12-13 08:24:11	Wrong	shreyaaaaa262	C++	View
+2022-12-13 08:13:33	Correct	dung689	C++	View
+2022-12-13 08:06:20	Correct	1priyansh25	C++	View
+2022-12-13 07:47:51	TLE	vedantbh3637	C++	View
+2022-12-13 06:31:47	Wrong	kevinlang9981	Java	View
+2022-12-13 06:11:52	Wrong	kevinlang9981	Java	View
+2022-12-13 06:06:50	Wrong	kevinlang9981	Java	View
+2022-12-13 05:59:45	Wrong	kevinlang9981	Java	View
+2022-12-13 03:53:55	Correct	ashokkumartelieee19	C++	View
+2022-12-13 02:49:59	Correct	samoil7bd8	Java	View
+2022-12-13 02:49:47	Compilation Error	samoil7bd8	Java	View
+2022-12-13 02:27:52	TLE	samoil7bd8	Java	View
+2022-12-13 02:27:47	Correct	devsoori2000	Java	View
+2022-12-13 02:24:24	TLE	pranoy_nandy	Python3	View
+2022-12-13 02:18:13	Wrong	pranoy_nandy	Python3	View
+2022-12-13 01:53:30	Wrong	asadityak6db	Java	View
+2022-12-13 01:50:01	Wrong	asadityak6db	Java	View
+2022-12-13 01:14:59	Correct	vermasourabh512	C++	View
+2022-12-13 01:12:37	Wrong	swastikgulati5	C++	View
+2022-12-13 00:59:46	Correct	mahesh8619	Java	View
+2022-12-13 00:59:14	Correct	mukeshkushwaha9000	C++	View
+2022-12-13 00:58:04	Correct	ankush_086	C++	View
+2022-12-13 00:57:19	Correct	adarsh2001singh	C++	View
+2022-12-13 00:54:19	Correct	raivaibpmil	C++	View
+2022-12-13 00:50:11	Correct	bachlorboy15	C++	View
+2022-12-13 00:49:44	RunTime Error	mahesh8619	Java	View
+2022-12-13 00:47:03	Wrong	raivaibpmil	C++	View
+2022-12-13 00:04:02	Correct	ankush_086	C++	View
+2022-12-13 00:03:53	TLE	akrcetcs	C++	View
+2022-12-13 00:03:36	Correct	ankush_086	C++	View
+2022-12-13 00:03:22	Wrong	akrcetcs	C++	View
+2022-12-13 00:02:32	TLE	akrcetcs	C++	View
+2022-12-13 00:02:31	Correct	ankush_086	C++	View
+2022-12-13 00:02:16	Wrong	ankush_086	C++	View
+2022-12-12 23:57:59	Correct	suryankaghosh19	C++	View
+2022-12-12 23:57:53	Correct	aadikeshari	C++	View
+2022-12-12 23:54:55	Correct	suryankaghosh19	C++	View
+2022-12-12 23:54:51	Correct	ankush_086	C++	View
+2022-12-12 23:54:39	Correct	ankush_086	C++	View
+2022-12-12 23:54:23	Correct	ankush_086	C++	View
+2022-12-12 23:52:51	Correct	ankush_086	C++	View
+2022-12-12 23:52:34	Correct	ankush_086	C++	View
+2022-12-12 23:52:21	Correct	ankush_086	C++	View
+2022-12-12 23:52:12	Correct	slakshit636	Java	View
+2022-12-12 23:49:08	TLE	slakshit636	Java	View
+2022-12-12 23:17:54	Correct	biledimi3l4y	Java	View
+2022-12-12 23:16:49	RunTime Error	biledimi3l4y	Java	View
+2022-12-12 23:15:21	Correct	nayanshree28022001	C++	View
+2022-12-12 23:14:51	Correct	ajaysharma343	C++	View
+2022-12-12 23:14:16	Correct	shejutikarmakar3	C++	View
+2022-12-12 23:13:19	TLE	arjain020502	C++	View
+2022-12-12 23:09:34	TLE	ananyaprabhu484	C++	View
+2022-12-12 23:07:49	TLE	bbbhowmik04	C++	View
+2022-12-12 23:07:08	Wrong	bbbhowmik04	C++	View
+2022-12-12 23:06:47	Correct	ghatetgfj4	Java	View
+2022-12-12 23:00:08	Correct	debanjandgt	Java	View
+2022-12-12 22:52:00	Correct	2017102	Java	View
+2022-12-12 22:40:29	TLE	ritikaniranjan25	Java	View
+2022-12-12 22:39:13	Correct	subhradippalit	C++	View
+2022-12-12 22:39:01	TLE	ritikaniranjan25	Java	View
+2022-12-12 22:30:36	Correct	bewayir451	Java	View
+2022-12-12 22:25:36	Correct	guptasarthak7266	C++	View
+2022-12-12 22:20:33	Correct	kanthwals	C++	View
+2022-12-12 22:20:22	Correct	sarthakgupta10	C++	View
+2022-12-12 22:19:46	Correct	vantary	C++	View
+2022-12-12 22:19:26	Correct	kanthwals	C++	View
+2022-12-12 22:16:13	TLE	bbbhowmik04	C++	View
+2022-12-12 22:14:50	TLE	sarthakgupta10	C++	View
+2022-12-12 22:13:02	TLE	siddarthagx6dn	C++	View
+2022-12-12 22:09:54	Wrong	vantary	C++	View
+2022-12-12 21:54:32	TLE	arjunn28	Python3	View
+2022-12-12 21:51:20	Correct	Sanjolly goyal	C++	View
+2022-12-12 21:49:07	Wrong	arjunn28	Python3	View
+2022-12-12 21:46:58	TLE	Sanjolly goyal	C++	View
+2022-12-12 21:43:42	Correct	m_venkatesh	C++	View
+2022-12-12 21:41:38	Wrong	arjunn28	Python3	View
+2022-12-12 21:38:19	TLE	donthupranavi	Java	View
+2022-12-12 21:35:04	TLE	donthupranavi	Java	View
+2022-12-12 21:34:03	Correct	adityadolui99	C++	View
+2022-12-12 21:32:23	TLE	donthupranavi	Java	View
+2022-12-12 21:29:02	TLE	donthupranavi	Java	View
+2022-12-12 21:28:35	TLE	dragontruoki	Python3	View
+2022-12-12 21:26:36	Wrong	Sanjolly goyal	C++	View
+2022-12-12 21:26:30	TLE	khushibhyvl1	Java	View
+2022-12-12 21:26:15	Wrong	Sanjolly goyal	C++	View
+2022-12-12 21:26:09	Correct	adityadolui99	C++	View
+2022-12-12 21:25:11	Correct	kalyanimsawai	C++	View
+2022-12-12 21:18:18	Correct	kanthwals	C++	View
+2022-12-12 21:18:06	Correct	chamanmandloi8717	C++	View
+2022-12-12 21:12:11	TLE	harleenkaur132435	Java	View
+2022-12-12 21:10:58	Correct	mukulgoswami304	C++	View
+2022-12-12 21:06:23	Wrong	mukulgoswami304	C++	View
+2022-12-12 21:05:07	Wrong	ashokkumartelieee19	C++	View
+2022-12-12 21:04:31	Wrong	ashokkumartelieee19	C++	View
+2022-12-12 20:59:39	Correct	harshad9066	Java	View
+2022-12-12 20:56:39	TLE	kirarshivcharan	Java	View
+2022-12-12 20:48:02	Correct	lovekeshkumar3535	C++	View
+2022-12-12 20:47:12	Correct	lovekeshkumar3535	C++	View
+2022-12-12 20:42:42	Correct	mohariyabunty	Java	View
+2022-12-12 20:40:18	Correct	riturajtri958f	C++	View
+2022-12-12 20:33:39	Wrong	riturajtri958f	C++	View
+2022-12-12 20:32:26	Correct	prasad89	C++	View
+2022-12-12 20:29:02	Correct	tejasayya8	Java	View
+2022-12-12 20:28:15	Wrong	riturajtri958f	C++	View
+2022-12-12 20:27:06	Correct	prasad89	C++	View
+2022-12-12 20:26:41	TLE	lovekeshkumar3535	C++	View
+2022-12-12 20:26:11	Wrong	prasad89	C++	View
+2022-12-12 20:25:46	Wrong	prasad89	C++	View
+2022-12-12 20:24:16	Correct	tejasayya8	Java	View
+2022-12-12 20:22:10	TLE	tejasayya8	Java	View
+2022-12-12 20:19:32	TLE	joysarkar8171	Java	View
+2022-12-12 20:17:59	Correct	arikatlapramod	C++	View
+2022-12-12 20:13:53	Wrong	kanthwals	C++	View
+2022-12-12 20:10:05	Wrong	ashokkumartelieee19	C++	View
+2022-12-12 20:08:40	Wrong	ashokkumartelieee19	C++	View
+2022-12-12 20:08:04	Correct	2002jhashivani	C++	View
+2022-12-12 20:05:33	Wrong	arikatlapramod	C++	View
+2022-12-12 20:04:18	TLE	tejasayya8	Java	View
+2022-12-12 20:02:37	Correct	parwezhai8ic	C++	View
+2022-12-12 20:02:21	TLE	parwezhai8ic	C++	View
+2022-12-12 20:02:12	TLE	parwezhai8ic	C++	View
+2022-12-12 19:59:47	Correct	parwezhai8ic	C++	View
+2022-12-12 19:58:40	Correct	parwezhai8ic	C++	View
+2022-12-12 19:55:11	Wrong	2002jhashivani	C++	View
+2022-12-12 19:40:02	TLE	eitzzmee	Python3	View
+2022-12-12 19:39:35	Wrong	eitzzmee	Python3	View
+2022-12-12 19:37:11	Correct	nimmanau4kx	C++	View
+2022-12-12 19:36:45	Wrong	nivagarwal64	Java	View
+2022-12-12 19:34:12	Correct	rishikeshdurale	C++	View
+2022-12-12 19:33:09	Correct	harsh9711904891	C++	View
+2022-12-12 19:31:28	TLE	eitzzmee	Python3	View
+2022-12-12 19:31:20	Wrong	rishikeshdurale	C++	View
+2022-12-12 19:30:35	TLE	user_px015m51z14	C++	View
+2022-12-12 19:27:47	TLE	eitzzmee	Python3	View
+2022-12-12 19:26:34	TLE	eitzzmee	Python3	View
+2022-12-12 19:25:04	Wrong	eitzzmee	Python3	View
+2022-12-12 19:24:32	Wrong	eitzzmee	Python3	View
+2022-12-12 19:23:59	TLE	rishikeshdurale	C++	View
+2022-12-12 19:20:08	TLE	eitzzmee	Python3	View
+2022-12-12 19:17:48	Correct	tanishgoyal16	Java	View
+2022-12-12 19:16:26	TLE	eitzzmee	Python3	View
+2022-12-12 19:15:23	Wrong	2017102	Java	View
+2022-12-12 19:13:56	Wrong	2017102	Java	View
+2022-12-12 19:11:42	Wrong	2017102	Java	View
+2022-12-12 19:06:26	Wrong	tejasayya8	Java	View
+Output Window
+Compilation Completed
+For Input: 
+4 6
+1 5 7 1
+Your Output: 
+0
+Expected Output: 
+2
+Output Difference
+02
+C++ (g++ 5.4)
+Average Time: 20m
+
+
+
+    result /= 2;
+
+
+
+Custom Input
+//User function template for C++
+
+class Solution{   
+
+public:
+
+    int getPairsCount(int arr[], int n, int k) {
+
+        // code here
+
+       unordered_map<int,int> m;
+
+       int count=0;
+
+    for(int i=0;i<n;i++){
+
+    int value = arr[i];
+
+   int neededvalue = k-value;
+
+   if(m.find(neededvalue)!= m.end()){
+
+               count+=m[neededvalue];
+
+//FREQUENCY OF THAT ELEMENT IN AN ARRAY
+
+           }
+
+          m[arr[i]]++;
+
+       }
+
+     return count;
+
+    }
+
+};
